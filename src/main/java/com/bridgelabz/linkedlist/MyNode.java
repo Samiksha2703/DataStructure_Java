ce1a6a59b72c12ca9814df655c3102fc1797656b
Samiksha2703 <shende.samiksha@rediffmail.com>
2021-03-25 15:24:33 +0530

    [Samiksha].ADD code to create simple linked list

---
+++ src/main/java/com/bridgelabz/linkedlist/MyNode.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.linkedlist;
+
+public class MyNode<K> {
+    private MyNode next;
+    private K key;
+
+    public MyNode getNext() {
+        return next;
+    }
+
+    public void setNext(MyNode next) {
+        this.next = next;
+    }
+
+    public MyNode(K key) {
+        this.key = key;
+        this.next = next;
+    }
+}
---
+++ src/test/java/com/bridgelabz/linkedlisttest/LinkedListTest.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.linkedlisttest;
+
+import com.bridgelabz.linkedlist.MyNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LinkedListTest {
+
+    @Test
+    public void whenPassThreeNumber_LinkedListShouldPassedTest() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        myFirstNode.setNext(mySecondNode);
+        mySecondNode.setNext(myThirdNode);
+        boolean result = myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode);
+        Assertions.assertTrue(result);
+    }
+}
